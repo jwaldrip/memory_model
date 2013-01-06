28534e64b46676a20f254a6b33a95d7cb8b3885a
Jason Waldrip <jason@waldrip.net>
2013-01-06 12:21:29 -0700

    refactored fieldable_spec to use memory_model

--- spec/memory_model/base/fieldable_spec.rb
+++ spec/memory_model/base/fieldable_spec.rb
@@ -2,28 +2,21 @@ require 'spec_helper'
 
 describe MemoryModel::Base::Fieldable do
 
-  let(:base_klass) do
-    Class.new do
-      include MemoryModel::Base::Fieldable
-    end
-  end
-  let(:other_klass) do
-    Class.new(base_klass)
-  end
-
+  let(:model_a) { Class.new(MemoryModel::Base) }
+  let(:model_b) { Class.new(MemoryModel::Base) }
 
   describe '.field' do
     it 'should add a field to fields' do
       field = :foo
-      base_klass.send :field, field
-      base_klass.fields.should include field
+      model_a.send :field, field
+      model_a.fields.should include field
     end
 
     it 'should not dirty other classes' do
       field = :bar
-      other_klass.send :field, field
-      other_klass.fields.should include field
-      base_klass.fields.should_not include field
+      model_a.send :field, field
+      model_a.fields.should include field
+      model_b.fields.should_not include field
     end
   end
 
