013c9bf47f5fc2f17ef5963d129b2398655ddd47
Jason Waldrip <jason@waldrip.net>
2013-10-17 01:27:01 -0600

    push latest benchmarks

--- spec/benchmark/benchmark.rb
+++ spec/benchmark/benchmark.rb
@@ -1,7 +1,8 @@
 require 'bundler'
 Bundler.setup(:default)
 require 'memory_model'
-require 'pry'
+require 'colorize'
+require 'faker'
 
 class Foo < MemoryModel::Base
 
@@ -18,36 +19,112 @@ class Foo < MemoryModel::Base
 
 end
 
-puts '',
-     '########## CURRENT BENCHMARK ##########',
-     ''
+def benchmark_average(count, name = nil, options={}, &block)
 
-def benchmark_average(count, name = nil, graph = false, &block)
-  times                                 = count.times.map do
+  nominal_range = options[:range] || (1.5..4)
+
+  puts '',
+       "Starting benchmark of #{name} on #{count} records",
+       '---------',
+       '| '.green + "< #{nominal_range.min}ms",
+       '| '.yellow + "#{nominal_range.to_s}ms",
+       '| '.red + "> #{nominal_range.max}ms",
+       '---------'
+  times = count.times.map do
+    options[:before_each].call if options[:before_each].is_a? Proc
     st = Time.now
     block.call
     (Time.now - st).tap do |seconds|
-      print (seconds * 10000).to_i.times.map {'.'}.join + "\n" if graph
+      milliseconds = seconds * 1000
+      color        = case milliseconds
+                     when 0..nominal_range.min
+                       :green
+                     when nominal_range
+                       :yellow
+                     else
+                       :red
+                     end
+      print '|'.colorize(color)
     end
   end
+  options[:after_all].call if options[:after_all].is_a? Proc
+  print "\n"
   total                                 = times.reduce(:+)
   shortest_milliseconds, shortest_index = times.map { |t| t * 1000 }.each_with_index.sort { |(timea, ia), (timeb, ib)| timea <=> timeb }.first
   longest_milliseconds, longest_index   = times.map { |t| t * 1000 }.each_with_index.sort { |(timea, ia), (timeb, ib)| timea <=> timeb }.reverse.first
   milliseconds_avg                      = (total / count) * 1000
-  puts "when executing #{name}",
+
+  color = case milliseconds_avg
+          when 0..nominal_range.min
+            :green
+          when nominal_range
+            :yellow
+          else
+            :red
+          end
+
+  puts '',
+       '------------------------------ BENCHMARK RESULTS ------------------------------',
+       '',
+       "when executing #{name}",
        "#{count} times",
-       "executions took an average of #{milliseconds_avg} milliseconds",
+       "executions took an average of #{milliseconds_avg} milliseconds".colorize(color),
        "and a total of #{total.round(2)} seconds",
-       "shortest was execution ##{shortest_index} lasting #{shortest_milliseconds} milliseconds",
-       "longest was execution ##{longest_index} lasting #{longest_milliseconds} milliseconds"
+       "shortest".green + " was execution ##{shortest_index} lasting #{shortest_milliseconds} milliseconds",
+       "longest".red + " was execution ##{longest_index} lasting #{longest_milliseconds} milliseconds",
+       '',
+       '###############################################################################'
+
+  GC.start
 
 end
 
-benchmark_average(1000, 'create') { Foo.create first_name: ['Tom', 'Alex', 'Jason'].sample }
+def gather_attributes!
+  $attributes = { first_name: Faker::Name.first_name,
+                  last_name:  Faker::Name.last_name,
+                  age:        Random.rand(0..100),
+                  email:      "#{Faker::Lorem.word}_#{SecureRandom.hex(12)}@example.org"
+  }
+end
 
-puts '',
-     'here\'s a sample record:',
-     Foo.sample.inspect
+def create_record!
+  Foo.create gather_attributes!
+end
 
-puts '',
-     '#######################################'
+def clear!
+  Foo.clear
+end
+
+n = 2500
+
+## Creates
+
+# Benchmark Create
+benchmark_average(n, '.create', before_each: -> { gather_attributes! }, after_all: -> { clear! }) do
+  Foo.create $attributes
+end
+
+## Reads
+
+# Benchmark Find
+require 'pry'
+benchmark_average(n, '.find', before_each: -> { create_record!; $id = Foo.sample.id }, after_all: -> { clear! }) { Foo.find($id) }
+
+# Benchmark Sample
+benchmark_average(n, '.sample', before_each: -> { create_record! }, after_all: -> { clear! }) { Foo.sample }
+
+# Benchmark Where (using index)
+benchmark_average(n, '.where (using index)', before_each: -> { create_record! }, after_all: -> { clear! }) { Foo.where(first_name: Faker::Name.first_name) }
+
+# Benchmark Where (using loading)
+benchmark_average(1000, '.where (using loading)', before_each: -> { create_record! }, after_all: -> { clear! }) { Foo.where(age: Random.rand(0..100)) }
+
+## Updates
+
+# Benchmark Update
+benchmark_average(n, '.update', before_each: -> { create_record! }, after_all: -> { clear! }) { Foo.sample.save }
+
+## Deletes
+
+# Benchmark Delete
+benchmark_average(n, '.delete', before_each: -> { 2.times { create_record! } }, after_all: -> { clear! }) { Foo.sample.delete }
